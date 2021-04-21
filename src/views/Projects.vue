1f400c4c99e91496878160586eb0ed18429f3541
sharonV33 <sharonveldman33@gmail.com>
2021-04-21 10:27:38 +0200

    add new project, Guess that Album

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -3,6 +3,9 @@
     <div class="sign">
       <span class="sign__word">My projects</span>
     </div>
+      <a class="card" href="/#/projects/capturingCorona">
+        <img :src="image[2]" alt="project screenshot">
+        Guess that Album</a>
         <a class="card" href="/#/projects/musicDiscovery">
             <img :src="image[0]" alt="project screenshot">
             Music Discovery</a>
@@ -10,10 +13,10 @@
             <img :src="image[1]" alt="project screenshot">
             capturing Corona</a>
         <a class="card" href="/#/projects/frontendApplications">
-            <img :src="image[2]" alt="project screenshot">
+            <img :src="image[3]" alt="project screenshot">
             frontend Applications</a>
         <a class="card" href="/#/projects/dinnerSelector">
-            <img :src="image[3]" alt="project screenshot">
+            <img :src="image[4]" alt="project screenshot">
             dinnerSelector</a>
 
   </div>
@@ -30,6 +33,7 @@
         image () {
             return [require(`../assets/images/musicDiscovery.png`),
                 require(`../assets/images/capturingCorona.png`),
+                require(`../assets/images/guessThatAlbum.png`),
                 require(`../assets/images/frontendApplications.png`),
                 require(`../assets/images/dinnerSelector.png`)]
         }
@@ -53,6 +57,7 @@
         display: flex;
         flex-direction: column;
         align-items: center;
+        max-width: 50em;
     }
     img {
         width: 100%;
