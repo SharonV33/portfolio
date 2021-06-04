fd5b51b4bfbf06ab55429297e529ed42a880fe50
SharonV33 <sharonveldman33@gmail.com>
2021-06-04 14:15:19 +0200

    update info

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,8 +7,8 @@
       My name is Sharon Veldman, I'm 23 years old. I currently live in Den Bosch with my dad and our two cats. In my free time I enjoy listening to metal music and watching scary movies.
       Normally I would frequently visit festivals but Corona has put a big halt to this. I'm also a student at the Hogeschool van Amsterdam. I study Communication and Multimedia design.
       I'm currently working on finishing a minor in web development and design.
-    </p>
-    <p class="card">
+        <br />
+        <br />
       During my internship I would like to improve my design skills and my knowledge of Javascript. I find it difficult to design new and interesting layouts and colours and fonts are still a mystery to me.
       I have been improving this skill in the last year but I still have a way to go. This is also the case for my Javascript skills. While already being much higher than a year ago,
       I feel like I could still improve a great deal.
